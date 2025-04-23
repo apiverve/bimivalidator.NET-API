57e4fdc159c94612c631be9ad100487223e88199
APIVerve Team <hello@apiverve.com>
2025-04-23 11:21:39 -0500

    Bug Fixes

---
+++ BIMIRecordValidatorQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class BIMIRecordValidatorQueryOptions {
+public string domain { get; set; }
+}
+
+}
